f61c472881727a8986d76fb649e4f72cd81fd1ff
aghermanHP <adrian.gherman1@gmail.com>
2020-04-23 16:20:20 +0300

    was aded raports and aded logint to inbox

--- lab2/front/src/views/Mails.vue
+++ lab2/front/src/views/Mails.vue
@@ -14,6 +14,48 @@
           </v-list-item-content>
         </v-list-item>
       </template>
+  <div class="text-center">
+    <v-bottom-sheet v-model="sheet" persistent>
+      <v-sheet class="text-center" height="100%">
+        <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="email from"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show2 ? 'text' : 'password'"
+            name="input-10-2"
+            label="Visible"
+            hint="At least 8 characters"
+            v-model="password"
+            class="input-group--focused"
+            @click:append="show2 = !show2"
+          ></v-text-field>
+        </v-col>
+        </v-row>
+        <v-btn
+          class="mt-6"
+          flat
+          color="error"
+          @click="getMails(email, password)"
+        >check inbox</v-btn>
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
   </v-card>
 </template>
 
@@ -21,23 +63,36 @@
   export default {
     data: () => ({
       apiLink: "http://localhost:5000/mail",
-      items: null
+      items: null,
+      show2: false,
+      sheet: true,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => ('The email and password you entered don\'t match'),
+        },
     }),
   
   mounted(){
-    this.getMails()
   },
   methods:{
-    getMails(){
+    getMails(mail, pass){
     this.axios.request(this.apiLink, {
       method: 'GET',
       params: {
-      password:'Ghera123Ad', 
-      email:'tsetsmtp@gmail.com'
+      password: pass, 
+      email: mail
       }
     }).then((response) => {
       this.items = response.data.mails
   console.log(response.data.status)
+  this.sheet = false
 }).catch((error) => {
   console.log(error)
 })
