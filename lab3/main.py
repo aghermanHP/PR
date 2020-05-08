d472d10a138fce6e497a9fc95b445eb3b43ba0b7
aghermanHP <adrian.gherman1@gmail.com>
2020-05-08 11:16:03 +0300

    was aded raport for lab3 and lab4

--- lab3/main.py
+++ lab3/main.py
@@ -1,7 +1,11 @@
 import argparse
+import requests
 from modules import index
+from modules import facebook
 
 parser = argparse.ArgumentParser(description='Process http methods')
+parser.add_argument('-email', type=str, help='Email address')
+parser.add_argument('-password', type=str, help='Login password')
 parser.add_argument("-getCats", action="store_true", help="getCats Method")
 parser.add_argument("-getVotes", action="store_true", help="get Votes of cats")
 parser.add_argument("-options", action="store_true", help="options of cats")
@@ -29,6 +33,23 @@ elif args.head:
 elif args.post:
     x = methods.post_vote(args.post[0], int(args.post[1]))
     print(x)
+elif args.email and args.password:
+    proxies = {
+        'http': '192.168.1.105:3128',
+        'https': '192.168.1.105:3128',
+    }
+    session = requests.session()
+    session.proxies = proxies
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0'
+    })
+
+    fb_dtsg, user_id, xs = facebook.login(session, args.email, args.password)
+
+    if user_id:
+        print('Facebook:{0}:{1}:{2}'.format(fb_dtsg, user_id, xs))
+    else:
+        print('Login Failed')
 
 
 
---
+++ lab3/modules/facebook.py
@@ -0,0 +1,32 @@
+import pyquery
+
+
+def login(session, email, password):
+    '''
+    Attempt to login to Facebook. Returns user ID, xs token and
+    fb_dtsg token. All 3 are required to make requests to
+    Facebook endpoints as a logged in user. Returns False if
+    login failed.
+    '''
+
+    # Navigate to Facebook's homepage to load Facebook's cookies.
+    response = session.get('https://m.facebook.com')
+
+    # Attempt to login to Facebook
+    response = session.post('https://m.facebook.com/login.php', data={
+        'email': email,
+        'pass': password
+    }, allow_redirects=False)
+
+    # If c_user cookie is present, login was successful
+    if 'c_user' in response.cookies:
+
+        # Make a request to homepage to get fb_dtsg token
+        homepage_resp = session.get('https://m.facebook.com/home.php')
+
+        dom = pyquery.PyQuery(homepage_resp.text.encode('utf8'))
+        fb_dtsg = dom('input[name="fb_dtsg"]').val()
+
+        return fb_dtsg, response.cookies['c_user'], response.cookies['xs']
+    else:
+        return False
